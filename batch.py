67fafbec2f39d7fc9b873558705d52695b5f8572
jeremy-ma <jeremy_ma@outlook.com>
2016-05-05 00:24:20 +1000

    added better parallelism

--- batch.py
+++ batch.py
@@ -3,10 +3,28 @@ import config
 from system import prototype_montecarlo_system
 import multiprocessing
 from functools import partial
+from frontend import frontend
+import os
+import time
 
 if __name__ == '__main__':
+    print time.time()
+    manager = frontend.DataManager(data_directory=os.path.join(config.data_directory, 'preprocessed'),
+                                   enrol_file=config.reddots_part4_enrol_female,
+                                   trial_file=config.reddots_part4_trial_female)
+
+    save_path = os.path.join(config.dropbox_directory, config.computer_id)
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    speaker_trials = manager.get_unique_trials()
+    all_trials = []
+    for _, trials in speaker_trials.iteritems():
+        all_trials.extend(trials)
+
     pool = multiprocessing.Pool(None)
-    map_function = partial(prototype_montecarlo_system.calculate_samples, num_iterations=100, num_gaussians=8)
-    r = pool.map(map_function, [('f0001',), ('f0002',)])
+    map_function = partial(prototype_montecarlo_system.calculate_samples,
+                           save_path=save_path, num_iterations=100, num_gaussians=8)
+    r = pool.map(map_function, all_trials)
     r.wait() # Wait on the results
     print "done"
+    print time.time()
--- system/prototype_montecarlo_system.py
+++ system/prototype_montecarlo_system.py
@@ -3,8 +3,10 @@ from varun.MCMC import MCMCRun
 import os
 import cPickle
 from frontend import frontend
-import sys
+import sys, pdb
 import config
+import time
+
 
 class MonteCarloSystem():
 
@@ -22,24 +24,23 @@ class MonteCarloSystem():
         """
         return MCMCRun(X, numRuns=self.num_iterations, numMixtures=self.num_mixtures)
 
-    def batch_compute_samples_instance(self, trial_list, destination_directory):
+    def compute_samples_save(self, trial, destination_directory):
         """
         Compute samples for given utterance trials and save to file
         :param trial_list:
         :param destination_directory:
         """
-        for trial in trial_list:
-            output_filename = os.path.basename(trial.feature_file).split('.')[0] + '.pickle'
-            destination_folder = trial.actual_speaker
-            output_filename = os.path.join(destination_directory, destination_folder, output_filename)
-            features = trial.get_data()
-            samples = self.get_samples(features)
+        output_filename = os.path.basename(trial.feature_file).split('.')[0] + '.pickle'
+        destination_folder = trial.actual_speaker
+        output_filename = os.path.join(destination_directory, destination_folder, output_filename)
+        features = trial.get_data()
+        samples = self.get_samples(features)
 
-            if not os.path.exists(os.path.dirname(output_filename)):
-                os.makedirs(os.path.dirname(output_filename))
+        if not os.path.exists(os.path.dirname(output_filename)):
+            os.makedirs(os.path.dirname(output_filename))
 
-            with open(output_filename, 'wb') as fp:
-                cPickle.dump(samples, fp, cPickle.HIGHEST_PROTOCOL)
+        with open(output_filename, 'wb') as fp:
+            cPickle.dump(samples, fp, cPickle.HIGHEST_PROTOCOL)
 
     def train_background(self, background_features):
         self.background_samples = self.get_samples(background_features)
@@ -49,28 +50,21 @@ class MonteCarloSystem():
             self.model_samples[speaker_id] = self.get_samples(features)
 
 
-def calculate_samples(speakers_to_sample, num_iterations, num_gaussians=8):
-
-    print "hello"
-
-    manager = frontend.DataManager(data_directory=os.path.join(config.data_directory, 'preprocessed'),
-                                   enrol_file=config.reddots_part4_enrol_female,
-                                   trial_file=config.reddots_part4_trial_female)
-
+def calculate_samples(trial, save_path, num_iterations, num_gaussians=8):
+    print "starting calculation"
     system = MonteCarloSystem(num_gaussians=num_gaussians, num_iterations=num_iterations)
-    save_path = os.path.join(config.dropbox_directory, config.computer_id)
-
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-
-    speaker_trials = manager.get_unique_trials()
-
-    import time
-
     start = time.time()
-    print speakers_to_sample
-
-    for speaker_id in speakers_to_sample:
-        system.batch_compute_samples_instance(speaker_trials[speaker_id], save_path)
-
+    system.compute_samples_save(trial, save_path)
     print time.time() - start
+
+if __name__ == '__main__':
+    manager = frontend.DataManager(data_directory=os.path.join(config.data_directory, 'preprocessed'),
+                               enrol_file=config.reddots_part4_enrol_female,
+                               trial_file=config.reddots_part4_trial_female)
+    speaker_trials = manager.get_unique_trials()
+    count = 0
+    for speaker_id, trials in speaker_trials.iteritems():
+        print "{0}: {1}".format(speaker_id, len(trials))
+        count += len(trials)
+
+    print count
