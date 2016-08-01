23af8acf4e651c4cdd20930841214632d1421bf8
jeremy <jeremy_ma@hotmail.com>
2016-07-31 18:11:00 -0700

    add function to reduce size of the system

--- evaluate.py
+++ evaluate.py
@@ -18,7 +18,7 @@ def evaluate_system(system, manager, n_jobs, save_path):
             if count % 50 == 0:
                 print "iteration {0}".format(count)
             answer_array.append(trial.answer)
-            likelihood_array.append(system.verify(trial.claimed_speaker, trial.get_data(), n_jobs, burn_in=10000, lag=100))
+            likelihood_array.append(system.verify(trial.claimed_speaker, trial.get_data(), n_jobs, burn_in=5000, lag=50))
 
     #save results
     np.save(os.path.join(save_path, 'scoresMHMC.npy'), likelihood_array)
@@ -60,20 +60,30 @@ def evaluate_MCMAP(system, manager, n_jobs, save_path):
     np.save(os.path.join(save_path, 'scoresMCMAP.npy'), likelihood_array)
     np.save(os.path.join(save_path, 'answersMCMAP.npy'), answer_array)
 
+def reduce_system(system, save_path):
+
+    for speaker_id, samples in system.model_samples.iteritems():
+        system.model_samples[speaker_id] = samples[::50]
+
+    with open(os.path.join(save_path, 'reducedSystem.pickle'), 'w') as fp:
+        cPickle.dump(system, fp, protocol=cPickle.HIGHEST_PROTOCOL)
+
 if __name__=='__main__':
     manager = frontend.DataManager(data_directory=os.path.join(config.data_directory, 'preprocessed'),
                                    enrol_file=config.reddots_part4_enrol_female,
                                    trial_file=config.reddots_part4_trial_female)
 
-    n_mixtures, n_runs = 8, 20000
+    n_mixtures, n_runs = 8, 5000
     data = manager.get_background_data()
     description = 'mcmc_gaussian_prior_gmmcovars'
     save_path = os.path.join(config.dropbox_directory, config.computer_id, description,
                              'gaussians' + str(n_mixtures), 'iterations' + str(n_runs))
-    filename = os.path.join(save_path, 'system' + '.pickle')
+    filename = os.path.join(save_path, 'system.pickle')
 
     with open(filename, 'r') as fp:
         system = cPickle.load(fp)
+
+    reduce_system(system, save_path)
 
     evaluate_system(system, manager, 1, save_path)
     evaluate_MCMAP(system, manager, 1, save_path)
