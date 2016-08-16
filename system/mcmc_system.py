ea25779706592a3da109abfe4c6a7f4d07c3f9a6
jeremy <jeremy_ma@hotmail.com>
2016-08-15 22:13:25 -0700

    try new averaging method for ais

--- system/mcmc_system.py
+++ system/mcmc_system.py
@@ -125,10 +125,10 @@ class AIS_System(MCSystem):
     def verify(self, claimed_speaker, features, n_jobs):
         ais_samples = self.model_samples[claimed_speaker]
         n_features = features.shape[1]
-        numerator = logsumexp([(logweight + gmm.log_likelihood(features, n_jobs)) / n_features
+        numerator = logsumexp([logweight + gmm.log_likelihood(features, n_jobs)
                                for gmm, logweight in ais_samples])
         denominator = logsumexp([logweight for _, logweight in ais_samples])
-        claimed = numerator - denominator
+        claimed = (numerator - denominator) / n_features
         background = self.ubm.log_likelihood(features, n_jobs)
         likelihood_ratio = claimed - background
 
